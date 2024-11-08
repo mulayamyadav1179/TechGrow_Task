f29727c67dded16ed74abb85f1a928576532fbab
mulayamyadav1179 <147404048+mulayamyadav1179@users.noreply.github.com>
2024-11-08 21:58:29 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+  alert('Thank you for reaching out to us!');
+});
+
